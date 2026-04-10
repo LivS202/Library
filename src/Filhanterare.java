3266132e75a26afc5d87f2ea670c2c9e1d22159d
merella.jansson <merella.jansson@elev.ntig.se>
2026-04-10 11:05:33 +0200

    initial project upload

--- src/Filhanterare.java
+++ src/Filhanterare.java
@@ -1,24 +1,47 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException; //ger felmeddelande ifall filen inte finns istället för att avsluta/krascha
 
-public class Filhanterare {
+  public class Filhanterare {
 
-    public static void readFile(ArrayList <Bok> lista) {
+    public static void readFile(ArrayList<Bok>lista) {
         try {
-            File file = new File("Book.txt");
-            Scanner filScanner = new Scanner(file); //?
+            File file = new File("books.txt");
+            Scanner filScanner = new Scanner(file);
 
-            while (filScanner.hasNextLine()) { //fortsätter köra loop ifall mer text finns
-                String titel = filScanner.nextLine();
-                int år = filScanner.nextInt();
-                filScanner.nextLine();
-                String författare = filScanner.nextLine();
-                lista.add(new Bok(titel, år, författare));
+         while (filScanner.hasNextLine()) {
+            String titel = filScanner.nextLine();
+            int år = filScanner.nextInt();
+            filScanner.nextLine();
+            String forfattare = filScanner.nextLine();
+
+            Bok b = new Bok(titel, år, forfattare);
+            lista.add(b);
         }
-    }catch (FileNotFoundException e){
-        System.out.println("Filen finns inte! ");
-      }
+         filScanner.close();
+
+    }catch (FileNotFoundException e) {
+            System.out.println("Ingen fil hittades! ");
+          }
+       }
+
+       public static void saveFile(ArrayList<Bok>lista) {
+
+        try {
+            FileWriter writer = new FileWriter("src/books.txt");
+
+            for (Bok b : lista) {
+                writer.write(b.titel + "\n");
+                writer.write(b.år + "\n");
+                writer.write(b.forfattare + "\n");
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Fel vid sparning ");
+        }
     }
 }
